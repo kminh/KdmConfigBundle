f8b230a22233de3eb27c6cae9a71c09c94c9a617
Khang Minh <mr.minhkhang@gmail.com>
2015-03-23 01:28:34 +0700

    allow parameters in setting function call

--- Twig/Extension/ConfigExtension.php
+++ Twig/Extension/ConfigExtension.php
@@ -24,22 +24,32 @@ class ConfigExtension extends \Twig_Extension
 
     protected $kernel;
 
-    public function __construct(KernelInterface $kernel, SettingManagerInterface $settings)
+    public function __construct(
+        KernelInterface $kernel,
+        SettingManagerInterface $settings)
     {
-        $this->kernel   = $kernel;
-        $this->settings = $settings;
+        $this->kernel     = $kernel;
+        $this->settings   = $settings;
     }
 
     public function getFunctions()
     {
         return array(
-            new \Twig_SimpleFunction('setting', array($this, 'getSetting'))
+            new \Twig_SimpleFunction('setting', array($this, 'getSetting'), array(
+                'needs_environment' => true
+            ))
         );
     }
 
-    public function getSetting($settingName)
+    public function getSetting(\Twig_Environment $twig, $settingName, array $parameters = array())
     {
         try {
+            // if we need to parse parameters, do it now
+            if (sizeof($parameters) > 0) {
+                return $twig->render('KdmSetting:' . $settingName, $parameters);
+            }
+
+            // or simply return the setting's value
             return $this->settings->get($settingName);
         } catch (\Exception $e) {
             // throw error if setting is not found in environments that are not prod
